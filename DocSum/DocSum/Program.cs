4ab38e21cbecc9993d5aad2288bebd87fd967446
sireesha <balusireesha29@gmail.com>
2024-06-19 15:32:12 +0530

    commit_15:31_19/06/2024

--- DocSum/DocSum/Program.cs
+++ DocSum/DocSum/Program.cs
@@ -3,6 +3,81 @@ using DocSumServices;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register HttpClient
+builder.Services.AddHttpClient();
+
+// Register DocSumRepo
+builder.Services.AddScoped<IDocSumRepo, DocSumRepo>();
+
+var configuration = builder.Configuration;
+
+// Register DocSumService
+builder.Services.AddScoped<IDocSumService>((provider) =>
+{
+    var textAnalyticsEndpoint = configuration["TextAnalytics:Endpoint"];
+    var textAnalyticsKey = configuration["TextAnalytics:Key"];
+    var docSumRepo = provider.GetRequiredService<IDocSumRepo>();
+    return new DocSumService(docSumRepo, textAnalyticsEndpoint, textAnalyticsKey);
+});
+
+// Register CosmosClient
+builder.Services.AddSingleton<CosmosClient>((provider) =>
+{
+    var endpointUri = configuration["CosmosDbSettings:EndpointUri"];
+    var primaryKey = configuration["CosmosDbSettings:PrimaryKey"];
+    var cosmosClientOptions = new CosmosClientOptions
+    {
+        ApplicationName = "YourAppName"
+    };
+    var loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    });
+    var cosmosClient = new CosmosClient(endpointUri, primaryKey, cosmosClientOptions);
+    cosmosClient.ClientOptions.ConnectionMode = ConnectionMode.Direct;
+    return cosmosClient;
+});
+
+var app = builder.Build();
+
+// Configure CORS
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin() // Allow access from all origins
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
+
+
+
+
+/*using DocSumRepository;
+using DocSumServices;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -60,4 +135,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+app.Run();*/
